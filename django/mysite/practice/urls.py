b971e86dd48504520d9cba345e6dba6d3e877ec4
In Tae woo <81672260+InTaewoo@users.noreply.github.com>
2021-09-13 16:46:38 +0900

    Add files via upload

--- django/mysite/practice/urls.py
+++ django/mysite/practice/urls.py
@@ -4,6 +4,13 @@ from . import views
 appname = 'practice'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    path('index.html',views.index),
+    path('charts.html',views.charts),
+    path('samsung.html', views.samsung),
+    path('sk_hynicx.html', views.sk_hynicx),
+    path('hyundai.html', views.hyundai),
+    path('lg_chem.html', views.lg_chem),
+
 
 ]
--- django/mysite/practice/views.py
+++ django/mysite/practice/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request,'index.html')
+
+def samsung(request):
+    return render(request,'samsung.html')
+
+def sk_hynicx(request):
+    return render(request,'sk_hynicx.html')
+
+def hyundai(request):
+    return render(request,'hyundai.html')
+
+def lg_chem(request):
+    return render(request,'lg_chem.html')
+
+def charts(request):
+    return render(request,'charts.html')
+
